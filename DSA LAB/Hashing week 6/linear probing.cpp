ff44d778043d80389fe897f939da1134ef75d7fa
Ayesha <aishakhanpk27@gmail.com>
2026-04-08 21:54:58 +0500

    Add linear probing implementation in C++
    Implement linear probing for hash table insertion and display.

---
+++ DSA LAB/Hashing week 6/linear probing.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int table[10];
+
+    // initialize
+    for(int i=0;i<10;i++)
+        table[i] = -1;
+
+    int keys[] = {23, 33, 43};
+
+    for(int k=0;k<3;k++)
+    {
+        int key = keys[k];
+        int index = key % 10;
+
+        int i = 0;
+        while(table[(index + i) % 10] != -1)
+            i++;
+
+        table[(index + i) % 10] = key;
+    }
+
+    // display
+    for(int i=0;i<10;i++)
+        cout << table[i] << " ";
+
+    return 0;
+}
